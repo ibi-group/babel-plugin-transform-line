3b8032005349b5b53aaffed761626ab7cb80b2aa
Steven Olmsted <steven.olmsted@ibigroup.com>
2018-11-25 11:43:09 -0500

    Update dependencies; don't use babel to build the babel plugin

--- test/babel-plugin-transform-line.js
+++ test/babel-plugin-transform-line.js
@@ -1,9 +1,11 @@
-import _chai from 'chai';
-import _mocha from 'mocha';
+'use strict';
+
+const _chai = require('chai'),
+    _mocha = require('mocha');
 
 _mocha.describe('__line', () => {
     _mocha.it('should represent the current line number', () => {
-        _chai.expect(__line).to.equal(6);
+        _chai.expect(__line).to.equal(8);
     });
 
     _mocha.it('should be undefined when it doesn\'t exist in the original source', () => {
